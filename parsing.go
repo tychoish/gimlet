91046f2479c7279ca1e27d6603ebab9c397be311
tycho garen <garen@tychoish.com>
2017-08-02 06:35:49 -0400

    cap requests

--- parsing.go
+++ parsing.go
@@ -7,9 +7,10 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-
 	yaml "gopkg.in/yaml.v2"
 )
+
+const maxRequestSize = 16 * 1024 * 1024 // 16 MB
 
 // GetVars is a helper method that processes an http.Request and
 // returns a map of strings to decoded strings for all arguments
@@ -26,7 +27,7 @@ func GetVars(r *http.Request) map[string]string {
 func GetJSON(r io.ReadCloser, data interface{}) error {
 	defer r.Close()
 
-	bytes, err := ioutil.ReadAll(r)
+	bytes, err := ioutil.ReadAll(&io.LimitedReader{R: r, N: maxRequestSize})
 	if err != nil {
 		return err
 	}
@@ -37,12 +38,12 @@ func GetJSON(r io.ReadCloser, data interface{}) error {
 // GetYAML parses YAML from a io.ReadCloser (e.g. http/*Request.Body
 // or http/*Response.Body) into an object specified by the
 // request. Used in handler functiosn to retreve and parse data
-// submitted by the client.
+// submitted by the client.u
 func GetYAML(r io.ReadCloser, data interface{}) error {
 	defer r.Close()
 
 	// TODO: limited reader
-	bytes, err := ioutil.ReadAll(r)
+	bytes, err := ioutil.ReadAll(&io.LimitedReader{R: r, N: maxRequestSize})
 	if err != nil {
 		return err
 	}
