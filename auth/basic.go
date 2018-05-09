7d38855419be589f92319d1c607f7e46147ee213
tycho garen <garen@tychoish.com>
2018-05-09 17:31:20 -0400

    fix compile and implement auth

--- auth/basic.go
+++ auth/basic.go
@@ -2,9 +2,8 @@ package auth
 
 import (
 	"fmt"
+	"net/http"
 	"sync"
-
-	"github.com/evergreen-ci/gimlet/auth"
 )
 
 type BasicUser struct {
@@ -26,27 +25,30 @@ func (u *BasicUser) Roles() []string {
 }
 
 type basicAuthenticator struct {
-	mu     sync.RWMutex
-	users  map[string]User
-	groups map[string]string
+	mu       sync.RWMutex
+	users    map[string]User
+	groups   map[string]string
+	tokeName string
 }
 
-func NewbasicAuthenticator(users []User, groups map[string]string) auth.Authenticator {
+func NewBasicAuthenticator(users []User, groups map[string]string, tokenName string) Authenticator {
 	if groups == nil {
 		groups = map[string]string{}
 	}
 
 	a := &basicAuthenticator{
-		groups: groups,
-		users:  map[string]User{},
+		groups:   groups,
+		users:    map[string]User{},
+		tokeName: tokenName,
 	}
 
 	for _, u := range users {
 		if u != nil && !u.IsNil() {
 			a.users[u.Username()] = u
 		}
-
 	}
+
+	return a
 }
 
 func (a *basicAuthenticator) CheckResourceAccess(u User, resource string) bool {
@@ -67,12 +69,11 @@ func (a *basicAuthenticator) CheckGroupAccess(u User, group string) bool {
 		return false
 	}
 
-	if u.GetAPIKey() != ur.GetAPIKey() {
-		return false
+	if u.GetAPIKey() == ur.GetAPIKey() {
+		return true
 	}
 
-	return
-
+	return false
 }
 
 func (a *basicAuthenticator) CheckAuthenticated(u User) bool {
@@ -87,3 +88,69 @@ func (a *basicAuthenticator) CheckAuthenticated(u User) bool {
 
 	return u.GetAPIKey() == ur.GetAPIKey()
 }
+func (a *basicAuthenticator) GetUserFromRequest(um UserManager, r *http.Request) (User, error) {
+	return nil, nil
+}
+
+/*
+func setRequestUser(r *http.Requst, u User) *http.Request { return r }
+
+func (a *basicAuthenticator) GetUserFromRequest(um UserManager, r *http.Request) (User, error) {
+	a.mu.RLock()
+	defer a.mu.RUnlock()
+
+	token := ""
+	var err error
+	// Grab token auth from cookies
+	for _, cookie := range r.Cookies() {
+		if cookie.Name == a.tokenName {
+			if token, err = url.QueryUnescape(cookie.Value); err == nil {
+				break
+			}
+		}
+	}
+
+	// Grab API auth details from header
+	var authDataAPIKey, authDataName string
+	if len(r.Header["Api-Key"]) > 0 {
+		authDataAPIKey = r.Header["Api-Key"][0]
+	}
+	if len(r.Header["Auth-Username"]) > 0 {
+		authDataName = r.Header["Auth-Username"][0]
+	}
+	if len(authDataName) == 0 && len(r.Header["Api-User"]) > 0 {
+		authDataName = r.Header["Api-User"][0]
+	}
+
+	if len(token) > 0 {
+		ctx := r.Context()
+		u, err := um.GetUserByToken(ctx, token)
+
+		if err != nil {
+			grip.Infof("Error getting user %s: %+v", authDataName, err)
+		} else {
+			// Get the user's full details from the DB or create them if they don't exists
+			if err != nil {
+				grip.Debug(message.WrapError(err, message.Fields{
+					"message": "error looking up user",
+					"user":    u.Username(),
+				}))
+			} else {
+				r = setRequestUser(r, dbUser)
+			}
+		}
+	} else if len(authDataAPIKey) > 0 {
+		dbUser, err := user.FindOne(user.ById(authDataName))
+		if dbUser != nil && err == nil {
+			if dbUser.APIKey != authDataAPIKey {
+				http.Error(rw, "Unauthorized - invalid API key", http.StatusUnauthorized)
+				return
+			}
+			r = setRequestUser(r, dbUser)
+		} else {
+			grip.Errorln("Error getting user:", err)
+		}
+	}
+
+}
+*/
