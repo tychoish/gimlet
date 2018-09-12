41766df9b21dec0d55f229f2b9d1dc0e5abcdf15
Brian Samek <bsamek@users.noreply.github.com>
2018-09-12 17:01:47 -0400

    EVG-5299 Implement GetUserByToken (#2)

--- ldap/ldap.go
+++ ldap/ldap.go
@@ -15,27 +15,54 @@ import (
 // implements the gimlet.Authenticator interface.
 type userService struct {
 	CreationOpts
-	conn *ldap.Conn
+	conn ldap.Client
 }
 
 // CreationOpts are options to pass to the service constructor.
 type CreationOpts struct {
-	URL  string
-	Port string
-	Path string
+	URL   string          // URL of the LDAP server
+	Port  string          // Port of the LDAP server
+	Path  string          // Path to users LDAP OU
+	Group string          // LDAP group to authorize users
+	Put   PutUserGetToken // Put user to cache
+	Get   GetUserByToken  // Get user from cache
+
+	connect connectFunc // connect changes connection behavior for testing
 }
 
-// NewUserService constructs a userService. It requires a URL and Port to the LDAP
-// server. It also requires a Path to user resources that can be passed to an LDAP query.
+// PutUserGetToken is a function provided by the client to cache users. If the user is already in the
+// cache, it returns the user's token from the cache. If the user is not in the cache, it generates,
+// saves, and returns a new token. Updating the user's TTL should happen in this function.
+type PutUserGetToken func(gimlet.User) (string, error)
+
+// GetUserByToken is a function provided by the client to retrieve cached users by token.
+// It returns an error if and only if there was an error retrieving the user from the cache.
+// It returns (<user>, true, nil) if the user is present in the cache and is valid.
+// It returns (<user>, false, nil) if the user is present in the cache but has expired.
+// It returns (nil, false, nil) if the user is not present in the cache.
+type GetUserByToken func(string) (gimlet.User, bool, error)
+
+type connectFunc func(url, port string) (ldap.Client, error)
+
+// NewUserService constructs a userService. It requires a URL and Port to the LDAP server. It also
+// requires a Path to user resources that can be passed to an LDAP query.
 func NewUserService(opts CreationOpts) (gimlet.UserManager, error) {
 	if err := opts.validate(); err != nil {
 		return nil, err
 	}
 	u := &userService{}
 	u.CreationOpts = CreationOpts{
-		URL:  opts.URL,
-		Port: opts.Port,
-		Path: opts.Path,
+		URL:   opts.URL,
+		Port:  opts.Port,
+		Path:  opts.Path,
+		Group: opts.Group,
+		Put:   opts.Put,
+		Get:   opts.Get,
+	}
+	if opts.connect == nil {
+		u.CreationOpts.connect = connect
+	} else {
+		u.CreationOpts.connect = opts.connect
 	}
 	return u, nil
 }
@@ -44,29 +71,51 @@ func (opts CreationOpts) validate() error {
 	if opts.URL == "" || opts.Port == "" || opts.Path == "" {
 		return errors.Errorf("URL ('%s'), Port ('%s'), and Path ('%s') must be provided", opts.URL, opts.Port, opts.Path)
 	}
+	if opts.Group == "" {
+		return errors.New("LDAP group cannot be empty")
+	}
+	if opts.Put == nil || opts.Get == nil {
+		return errors.New("Put and Get must not be nil")
+	}
 	return nil
 }
 
-func (*userService) GetUserByToken(context.Context, string) (gimlet.User, error) {
-	return nil, errors.New("not yet implemented")
+func (u *userService) GetUserByToken(_ context.Context, token string) (gimlet.User, error) {
+	user, valid, err := u.Get(token)
+	if err != nil {
+		return nil, errors.Wrap(err, "problem getting cached user")
+	}
+	if user == nil {
+		return nil, errors.New("token is not present in cache")
+	}
+	if !valid {
+		if err := u.authorize(user.Username()); err != nil {
+			return nil, errors.Wrap(err, "could not authorize user")
+		}
+		if _, err := u.Put(user); err != nil {
+			return nil, errors.Wrap(err, "problem putting user in cache")
+		}
+	}
+	return user, nil
 }
-func (*userService) CreateUserToken(string, string) (string, error) {
+
+func (u *userService) CreateUserToken(string, string) (string, error) {
 	return "", errors.New("not yet implemented")
 }
-func (*userService) GetLoginHandler(url string) http.HandlerFunc { return nil }
-func (*userService) GetLoginCallbackHandler() http.HandlerFunc   { return nil }
-func (*userService) IsRedirect() bool                            { return false }
-func (*userService) GetUserByID(string) (gimlet.User, error) {
+func (u *userService) GetLoginHandler(url string) http.HandlerFunc { return nil }
+func (u *userService) GetLoginCallbackHandler() http.HandlerFunc   { return nil }
+func (u *userService) IsRedirect() bool                            { return false }
+func (u *userService) GetUserByID(string) (gimlet.User, error) {
 	return nil, errors.New("not yet implemented")
 }
-func (*userService) GetOrCreateUser(gimlet.User) (gimlet.User, error) {
+func (u *userService) GetOrCreateUser(gimlet.User) (gimlet.User, error) {
 	return nil, errors.New("not yet implemented")
 }
 
 // authenticate returns nil if the user and password are valid, an error otherwise.
 func (u *userService) authenticate(user, password string) error {
-	if err := u.connect(); err != nil {
-		return errors.Wrap(err, "could not connect to LDAP server")
+	if err := u.ensureConnected(); err != nil {
+		return errors.Wrap(err, "problem connecting to ldap server")
 	}
 	if err := u.login(user, password); err != nil {
 		return errors.Wrap(err, "failed to validate user")
@@ -74,25 +123,35 @@ func (u *userService) authenticate(user, password string) error {
 	return nil
 }
 
-// authorize returns nil if the user is a member of the group, an error otherwise.
-func (u *userService) authorize(user, group string) error {
-	if err := u.connect(); err != nil {
-		return errors.Wrap(err, "could not connect to LDAP server")
+// authorize returns nil if the user is a member of u.Group, an error otherwise.
+func (u *userService) authorize(user string) error {
+	if err := u.ensureConnected(); err != nil {
+		return errors.Wrap(err, "problem connecting to ldap server")
 	}
-	if err := u.isMemberOf(user, group); err != nil {
+	if err := u.isMemberOf(user, u.Group); err != nil {
 		return errors.Wrap(err, "failed to validate user")
 	}
 	return nil
 }
 
-func (u *userService) connect() error {
-	tlsConfig := &tls.Config{ServerName: u.URL}
-	var err error
-	u.conn, err = ldap.DialTLS("tcp", fmt.Sprintf("%s:%s", u.URL, u.Port), tlsConfig)
-	if err != nil {
-		return errors.Wrapf(err, "problem connecting to ldap server %s:%s", u.URL, u.Port)
+func (u *userService) ensureConnected() error {
+	if u.conn == nil {
+		conn, err := u.connect(u.URL, u.Port)
+		if err != nil {
+			return errors.Wrap(err, "could not connect to LDAP server")
+		}
+		u.conn = conn
 	}
 	return nil
+}
+
+func connect(url, port string) (ldap.Client, error) {
+	tlsConfig := &tls.Config{ServerName: url}
+	conn, err := ldap.DialTLS("tcp", fmt.Sprintf("%s:%s", url, port), tlsConfig)
+	if err != nil {
+		return nil, errors.Wrapf(err, "problem connecting to ldap server %s:%s", url, port)
+	}
+	return conn, nil
 }
 
 func (u *userService) login(user, password string) error {
--- ldap/ldap_test.go
+++ ldap/ldap_test.go
@@ -1,46 +1,214 @@
 package ldap
 
 import (
+	"context"
+	"crypto/tls"
 	"testing"
+	"time"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/evergreen-ci/gimlet"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/suite"
+	ldap "gopkg.in/ldap.v2"
 )
 
-func TestLDAPConstructorRequiresNonEmptyArgs(t *testing.T) {
-	assert := assert.New(t)
-
-	l, err := NewUserService(CreationOpts{"foo", "bar", "baz"})
-	assert.NotNil(l)
-	assert.NoError(err)
-
-	l, err = NewUserService(CreationOpts{"", "bar", "baz"})
-	assert.Nil(l)
-	assert.Error(err)
-
-	l, err = NewUserService(CreationOpts{"foo", "", "baz"})
-	assert.Nil(l)
-	assert.Error(err)
-
-	l, err = NewUserService(CreationOpts{"foo", "bar", ""})
-	assert.Nil(l)
-	assert.Error(err)
+type LDAPSuite struct {
+	um gimlet.UserManager
+	suite.Suite
 }
 
-// This test requires an LDAP server. Uncomment to test.
-//
-// func TestLDAPIntegration(t *testing.T) {
-// 	const (
-// 		url      = ""
-// 		port     = ""
-// 		path     = ""
-// 		user     = ""
-// 		password = ""
-// 		group    = ""
-// 	)
-// 	assert := assert.New(t)
-// 	l, err := NewUserService(CreationOpts{url, port, path})
-// 	assert.NotNil(l)
-// 	assert.NoError(err)
-// 	assert.NoError(l.authenticate(user, password))
-// 	assert.NoError(l.authorize(user, group))
-// }
+func TestLDAPSuite(t *testing.T) {
+	suite.Run(t, new(LDAPSuite))
+}
+
+func (s *LDAPSuite) SetupTest() {
+	var err error
+	s.um, err = NewUserService(CreationOpts{
+		URL:     "url",
+		Port:    "port",
+		Path:    "path",
+		Group:   "10gen",
+		Put:     mockPut,
+		Get:     mockGet,
+		connect: mockConnect,
+	})
+	s.Require().NotNil(s.um)
+	s.Require().NoError(err)
+}
+
+func mockConnect(url, port string) (ldap.Client, error) {
+	return &mockConn{}, nil
+}
+
+type mockConn struct{}
+
+func (m *mockConn) Start()                               { return }
+func (m *mockConn) StartTLS(config *tls.Config) error    { return nil }
+func (m *mockConn) Close()                               { return }
+func (m *mockConn) SetTimeout(time.Duration)             { return }
+func (m *mockConn) Bind(username, password string) error { return nil }
+func (m *mockConn) SimpleBind(simpleBindRequest *ldap.SimpleBindRequest) (*ldap.SimpleBindResult, error) {
+	return nil, nil
+}
+func (m *mockConn) Add(addRequest *ldap.AddRequest) error             { return nil }
+func (m *mockConn) Del(delRequest *ldap.DelRequest) error             { return nil }
+func (m *mockConn) Modify(modifyRequest *ldap.ModifyRequest) error    { return nil }
+func (m *mockConn) Compare(dn, attribute, value string) (bool, error) { return false, nil }
+func (m *mockConn) PasswordModify(passwordModifyRequest *ldap.PasswordModifyRequest) (*ldap.PasswordModifyResult, error) {
+	return nil, nil
+}
+func (m *mockConn) Search(searchRequest *ldap.SearchRequest) (*ldap.SearchResult, error) {
+	return &ldap.SearchResult{
+		Entries: []*ldap.Entry{
+			&ldap.Entry{
+				Attributes: []*ldap.EntryAttribute{
+					&ldap.EntryAttribute{
+						Values: []string{"10gen"},
+					},
+				},
+			},
+		},
+	}, nil
+}
+func (m *mockConn) SearchWithPaging(searchRequest *ldap.SearchRequest, pagingSize uint32) (*ldap.SearchResult, error) {
+	return nil, nil
+}
+
+type mockUser struct{ name string }
+
+func (u *mockUser) DisplayName() string { return "" }
+func (u *mockUser) Email() string       { return "" }
+func (u *mockUser) Username() string    { return u.name }
+func (u *mockUser) GetAPIKey() string   { return "" }
+func (u *mockUser) Roles() []string     { return []string{} }
+
+func mockPut(u gimlet.User) (string, error) {
+	return u.Username(), nil
+}
+
+type getController int
+
+const (
+	getErr getController = iota
+	getValidUser
+	getExpiredUser
+	getMissingUser
+)
+
+var getControl = getErr
+
+func mockGet(token string) (gimlet.User, bool, error) {
+	if getControl == getErr {
+		return nil, false, errors.New("error getting user")
+	}
+	if getControl == getValidUser {
+		return &mockUser{name: token}, true, nil
+	}
+	if getControl == getExpiredUser {
+		return &mockUser{name: token}, false, nil
+	}
+	return nil, false, nil
+}
+
+func (s *LDAPSuite) TestLDAPConstructorRequiresNonEmptyArgs() {
+	l, err := NewUserService(CreationOpts{
+		URL:   "url",
+		Port:  "port",
+		Path:  "path",
+		Group: "group",
+		Put:   mockPut,
+		Get:   mockGet,
+	})
+	s.NotNil(l)
+	s.NoError(err)
+
+	l, err = NewUserService(CreationOpts{
+		URL:   "",
+		Port:  "port",
+		Path:  "path",
+		Group: "group",
+		Put:   mockPut,
+		Get:   mockGet,
+	})
+	s.Nil(l)
+	s.Error(err)
+
+	l, err = NewUserService(CreationOpts{
+		URL:   "url",
+		Port:  "",
+		Path:  "path",
+		Group: "group",
+		Put:   mockPut,
+		Get:   mockGet,
+	})
+	s.Nil(l)
+	s.Error(err)
+
+	l, err = NewUserService(CreationOpts{
+		URL:   "url",
+		Port:  "port",
+		Path:  "",
+		Group: "group",
+		Put:   mockPut,
+		Get:   mockGet,
+	})
+	s.Nil(l)
+	s.Error(err)
+
+	l, err = NewUserService(CreationOpts{
+		URL:   "url",
+		Port:  "port",
+		Path:  "path",
+		Group: "",
+		Put:   mockPut,
+		Get:   mockGet,
+	})
+	s.Nil(l)
+	s.Error(err)
+
+	l, err = NewUserService(CreationOpts{
+		URL:   "url",
+		Port:  "port",
+		Path:  "path",
+		Group: "group",
+		Put:   nil,
+		Get:   mockGet,
+	})
+	s.Nil(l)
+	s.Error(err)
+
+	l, err = NewUserService(CreationOpts{
+		URL:   "url",
+		Port:  "port",
+		Path:  "path",
+		Group: "group",
+		Put:   mockPut,
+		Get:   nil,
+	})
+	s.Nil(l)
+	s.Error(err)
+}
+
+func (s *LDAPSuite) TestGetUserByToken() {
+	ctx := context.Background()
+
+	getControl = getErr
+	u, err := s.um.GetUserByToken(ctx, "erh")
+	s.Error(err)
+	s.Nil(u)
+
+	getControl = getValidUser
+	u, err = s.um.GetUserByToken(ctx, "erh")
+	s.NoError(err)
+	s.Equal("erh", u.Username())
+
+	getControl = getExpiredUser
+	u, err = s.um.GetUserByToken(ctx, "erh")
+	s.NoError(err)
+	s.Equal("erh", u.Username())
+
+	getControl = getMissingUser
+	u, err = s.um.GetUserByToken(ctx, "erh")
+	s.Error(err)
+	s.Nil(u)
+}
